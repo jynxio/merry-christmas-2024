4b4cb70b9d2bf510e30db65fc9c126ef466f491d
jynxio <jinxiaomatrix@gmail.com>
2024-12-25 01:36:26 +0800

    refactor: remove router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import '@/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import RouterProvider from './router';
+import App from './app';
 
 const root = document.getElementById('root')!;
 
 ReactDOM.createRoot(root).render(
     <React.StrictMode>
-        <RouterProvider />
+        <App />
     </React.StrictMode>,
 );
